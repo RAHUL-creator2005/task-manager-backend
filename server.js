d34d3c3d71c5a05c4f937deafce98ac3ebff0b6b
RAHUL-creator2005 <rahulkeerthivasan61@gmail.com>
2026-02-25 22:43:33 +0530

    new route added

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
+
 // Define Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
